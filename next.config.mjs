9792fbbbc45f48f99d304d4693d4d1b342990bff
sheikh-alamin <mimoalamin@gmail.com>
2026-04-17 03:16:45 +0600

    site name changed

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  // devIndicators: false
   // https://randomuser.me/api/portraits/women/44.jpg
   reactCompiler: true,
   images: {
--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
+import { FaHome } from "react-icons/fa";
 
 const NotFoundPage = () => {
     return (
     <div className="mx-auto text-center py-10 space-y-4">
-        <h2>Not Found</h2>
-        <p>Could not find requested resource</p>
-        <Link href="/"><button className="btn btn-primary">Back to home</button></Link>
+        <h1 className="text-[150px] font-extrabold text-[#244D3F]">404</h1>
+        <h2 className="text-[20px] font-bold text-[#244D3F]">Page Not Found</h2>
+        <p>Looks like this friendship link is broken. The page <br /> you are looking for does not exist or has been removed</p>
+        <Link href="/"><button className="btn bg-[#244D3F] text-white"><FaHome/> Back to home</button></Link>
     </div>
     );
 };
