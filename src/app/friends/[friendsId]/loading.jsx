66481676f0361330af1c8642f3d5e4bb4d20a1a7
sheikh-alamin <mimoalamin@gmail.com>
2026-04-17 21:37:58 +0600

    loading functional done

---
+++ src/app/friends/[friendsId]/loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="flex justify-center items-center h-screen font-bold text-5xl">
+      <span className="loading loading-dots loading-xl"></span>
+    </div>
+  );
+};
+
+export default loading;
